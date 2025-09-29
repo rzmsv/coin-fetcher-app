08fddb4feb1cf642207392d23204410bdbed3d6d
rmussavi <rmussavi@gmail.com>
2025-09-29 21:44:43 +0330

    feat: Add context wait to repositories

--- internal/adapters/repository/postgres/price.postgres.go
+++ internal/adapters/repository/postgres/price.postgres.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"context"
 	"time"
 
 	"github.com/username/coin-fetcher-app/internal/domain"
@@ -18,12 +19,16 @@ func NewPriceRepository(db *gorm.DB) domain.PriceRepository {
 }
 
 func (p *PriceRepository) Save(Coin domain.Coin) error {
-	return p.DB.Create(&Coin).Error
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return p.DB.WithContext(ctx).Create(&Coin).Error
 }
 
 func (p *PriceRepository) GetLastPrice(coin string) (domain.Coin, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	var Coin domain.Coin
-	err := p.DB.Where("coin = ?", coin).Order("timestamp DESC").First(&Coin).Error
+	err := p.DB.WithContext(ctx).Where("coin = ?", coin).Order("timestamp DESC").First(&Coin).Error
 	if err != nil {
 		return domain.Coin{}, err
 	}
@@ -31,10 +36,12 @@ func (p *PriceRepository) GetLastPrice(coin string) (domain.Coin, error) {
 
 }
 func (p *PriceRepository) GetAveragePrice(since time.Time, coin string) (float64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	var Result struct {
 		AvgPrice float64
 	}
-	err := p.DB.Model(&domain.Coin{}).Select("AVG(price) as avg_price").Where("coin = ? AND timestamp >= ?", coin, since).Scan(&Result).Error
+	err := p.DB.WithContext(ctx).Model(&domain.Coin{}).Select("AVG(price) as avg_price").Where("coin = ? AND timestamp >= ?", coin, since).Scan(&Result).Error
 	if err != nil {
 		return 0, err
 	}
